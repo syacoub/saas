14264cc5f5e570674591ff5e2c8a5cf36bc3f51e
Shady Yacoub <shady_@aucegypt.edu>
2012-08-05 04:20:25 +0200

    added ratings query parameter to sorting urls

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
     @sort_by = params[:sort_by]
     @all_ratings = Movie.all_ratings
     @movies = @included_ratings ? Movie.where(["rating IN (?)", @included_ratings]).find(:all, :order => @sort_by ? @sort_by : nil) : []
- 
+    @included_ratings_hash = params[:ratings] ? params[:ratings] : nil
   end
 
   def new
